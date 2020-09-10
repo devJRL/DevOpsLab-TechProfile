b1999ddedb89a718f6106af282984bfff6025444
devJRL <dev2sponge@gmail.com>
2020-09-11 00:20:59 +0900

    component - title

---
+++ next/components/home/title.tsx
@@ -0,0 +1,13 @@
+import "./title.scss";
+
+const Title = () => {
+  return (
+    <div className="title">
+      <div className="title__main">SP.ON.GE</div>
+      <div className="title__bar"></div>
+      <div className="title__sub">Speciality on Generality</div>
+    </div>
+  );
+};
+
+export default Title;
--- next/pages/index.tsx
+++ next/pages/index.tsx
@@ -1,8 +1,10 @@
 // REF | https://github.com/vercel/next-learn-starter/blob/master/typescript-final/pages/index.tsx
 import "@/public/_global.scss";
 
+import Title from "@/components/home/title";
+
 const Home = () => {
-  return <h1>Hello, world!</h1>; //;
+  return <Title />;
 };
 
 export default Home;
