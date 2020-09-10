57b471702f156bbe7c201a10dbaf6dedf7da8d81
devJRL <dev2sponge@gmail.com>
2020-09-10 22:53:48 +0900

    add scss: reset & fonts

---
+++ next/pages/_document.tsx
@@ -0,0 +1,32 @@
+// https://nextjs.org/docs/advanced-features/custom-document#typescript
+import Document, {
+  DocumentContext,
+  Html,
+  Head,
+  Main,
+  NextScript,
+} from "next/document";
+
+class MyDocument extends Document {
+  static async getInitialProps(ctx: DocumentContext) {
+    const initialProps = await Document.getInitialProps(ctx);
+    return { ...initialProps };
+  }
+
+  render() {
+    return (
+      <Html lang="en">
+        <Head>
+          <meta charSet="utf-8" />
+          {/* <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/reset-css@5.0.1/reset.min.css"/> */}
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
--- next/pages/index.tsx
+++ next/pages/index.tsx
@@ -1,4 +1,6 @@
 // REF | https://github.com/vercel/next-learn-starter/blob/master/typescript-final/pages/index.tsx
+import "@/public/_global.scss";
+
 const Home = () => {
   return <h1>Hello, world!</h1>; //;
 };
